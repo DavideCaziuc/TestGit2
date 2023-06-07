fec256602c8c10e71f532b1c7ec426b06e1beb1e
DavideCaziuc <davidecaziuc3@gmail.com>
2023-06-07 10:55:07 +0200

    Modifica main

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Avvio del programma");
+		System.out.println("Svolgimento del programma");
+		System.out.println("Fine del programma");
 
 	}
 
