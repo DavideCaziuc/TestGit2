2cbd83b2039317bb85c179d580a0ae348a1d96be
DavideCaziuc <104724334+DavideCaziuc@users.noreply.github.com>
2023-06-07 11:40:53 +0200

    Update Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Avvio del programma");
 		System.out.println("Svolgimento del programma");
+		System.out.println("Fasa 1 del programma");
 		System.out.println("Fine del programma");
 
 	}
